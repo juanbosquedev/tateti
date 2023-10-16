4954fb361edd5bf6d3ab6438da7a7bd2b0a5a62a
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-10-15 21:01:02 -0300

    main structure

--- src/components/TaTeTi.jsx
+++ src/components/TaTeTi.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+// import React from 'react'
 import "./TaTeTi.css"
+import circle from "../assets/circle.png"
+import cross from "../assets/cross.png"
+
 const TaTeTi = () => {
   return (
-    <div>TaTeTi</div>
+    <div className="container">
+        <h1 className="title">Ta Te Ti Game in</h1>
+        <h1 className="board"></h1>
+        <button className="reset">Reset</button>
+        <img src={circle}/>
+        <img src={cross}/>
+    </div>
   )
 }
 
